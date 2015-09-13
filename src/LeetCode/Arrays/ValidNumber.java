1db0728078446139d7a7d5c46e9b8252a628fc81
Prashant Golash <prashant.golash@gmail.com>
2015-09-13 00:21:45 -0700

    Valid Number added.

---
+++ src/LeetCode/Arrays/ValidNumber.java
@@ -0,0 +1,90 @@
+package LeetCode.Arrays;
+
+/**
+ * Created by prashantgolash on 9/13/15.
+ */
+public class ValidNumber {
+    public boolean isNumber(String s) {
+        if (s==null ){
+            return false;
+        }
+        // trim off head and tail zeros which not affect result depend on question
+        s=s.trim();
+        if(s.length()==0){
+            return false;
+        }
+        boolean hasNum=false;
+
+        boolean canSign=true;
+
+        boolean canDot=true;
+
+        boolean canE=false;
+        boolean hasE=false;
+
+        int i=0;
+        while(i<s.length()){
+            char c=s.charAt(i++);
+
+            if (c==' '){
+
+                return false;
+
+            }
+
+            if (c=='+' ||c=='-'){
+                if (!canSign){
+                    return false;
+                }
+                canSign=false;
+
+                continue;
+            }
+
+            if (c=='.'){
+                if (!canDot){
+                    return false;
+                }
+                canDot=false;
+                canSign=false;
+                continue;
+            }
+
+            if (c=='e'){
+
+                if (!canE||hasE){
+                    return false;
+                }
+
+                canE=false;
+                hasE=true;
+                hasNum=false;
+                canDot=false;
+                canSign=true;
+
+                continue;
+            }
+
+
+            if (c>='0' && c<='9'){
+                hasNum=true;
+
+                if (!canE&&!hasE){
+                    canE=true;
+                }
+
+                canSign=false;
+
+
+
+            }
+            else{
+                return false;
+            }
+
+
+        }
+
+        return hasNum;
+    }
+}
