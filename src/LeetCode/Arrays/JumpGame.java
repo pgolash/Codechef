af7720f8b6e75e6db897282fafb95928b335e4d1
Prashant Golash <prashant.golash@gmail.com>
2015-09-28 23:15:58 -0700

    Jump Game

---
+++ src/LeetCode/Arrays/JumpGame.java
@@ -0,0 +1,31 @@
+package LeetCode.Arrays;
+
+/**
+ * Created by prashantgolash on 9/28/15.
+ */
+public class JumpGame {
+    public boolean canJump(int[] A) {
+
+        if(A.length <= 1)
+            return true;
+
+        int max = A[0]; //max stands for the largest index that can be reached.
+
+        for(int i=0; i<A.length; i++){
+            //if not enough to go to next
+            if(max <= i && A[i] == 0)
+                return false;
+
+            //update max
+            if(i + A[i] > max){
+                max = i + A[i];
+            }
+
+            //max is enough to reach the end
+            if(max >= A.length-1)
+                return true;
+        }
+
+        return false;
+    }
+}
