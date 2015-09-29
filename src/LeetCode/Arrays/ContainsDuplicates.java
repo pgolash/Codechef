8a2880e942bb5a569c953cbfb3341dc8e5dc88f6
Prashant Golash <prashant.golash@gmail.com>
2015-09-28 22:31:39 -0700

    Contain Duplicates added.

---
+++ src/LeetCode/Arrays/ContainsDuplicates.java
@@ -0,0 +1,28 @@
+package LeetCode.Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by prashantgolash on 9/28/15.
+ */
+public class ContainsDuplicates {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        } else {
+            Map<Integer, Object> m = new HashMap<>();
+            boolean flag = false;
+
+            for (int i : nums) {
+                if (m.containsKey(i)) {
+                    flag = true;
+                    break;
+                } else {
+                    m.put(i, null);
+                }
+            }
+            return flag;
+        }
+    }
+}
