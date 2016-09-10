628c52efa0eb2f80f7481b165851015339de7d2e
Prashant Golash <prashant.golash@gmail.com>
2016-09-09 22:03:23 -0700

    Update MultiplyStrings.java

--- src/LeetCode/Strings/MultiplyStrings.java
+++ src/LeetCode/Strings/MultiplyStrings.java
@@ -38,3 +38,122 @@ public class MultiplyStrings {
         return sb.toString();
     }
 }
+
+ public String multiply(String num1, String num2) {
+        if (num1 == null || num2 == null) {
+            return null;
+        }
+
+        int sIdx = num2.length() - 1;
+        int placeTenCnt = 0;
+        int[] ans = null;
+
+        while (sIdx >= 0) {
+            int[] temp = multiply(num1, num2.charAt(sIdx) - '0', placeTenCnt);
+            ans = add(ans, temp);
+            sIdx--;
+            placeTenCnt++;
+        }
+
+        return getString(ans);
+
+    }
+
+    public int[] multiply(String num1, int num2, int placeTenCnt) {
+        int length = num1.length() + placeTenCnt + 1;
+        int[] ans = new int[length];
+
+        int sIdx = num1.length() - 1;
+        int tempIdx = length - 1;
+        int sum;
+        int carry = 0;
+
+        while (placeTenCnt > 0) {
+            ans[tempIdx] = 0;
+            tempIdx--;
+            placeTenCnt--;
+        }
+
+        while (sIdx >= 0) {
+            sum = (num1.charAt(sIdx) - '0') * num2 + carry;
+            ans[tempIdx] = sum % 10;
+            carry = sum / 10;
+            sIdx--;
+            tempIdx--;
+        }
+
+        if (carry >= 1) {
+            ans[tempIdx] = carry;
+            tempIdx--;
+        }
+
+        return Arrays.copyOfRange(ans, tempIdx + 1, ans.length);
+    }
+
+    public int[] add(int[] a, int []b) {
+        if (a == null && b == null) {
+            return null;
+        } else if (a == null) {
+            return b;
+        } else if (b == null) {
+            return a;
+        } else {
+            int aIdx = a.length - 1;
+            int bIdx = b.length - 1;
+            int sum;
+            int carry = 0;
+
+            while (aIdx >= 0 || bIdx >= 0) {
+                sum = 0;
+
+                if (aIdx >= 0) {
+                    sum += a[aIdx];
+                }
+
+                if (bIdx >= 0) {
+                    sum += b[bIdx];
+                }
+
+                sum += carry;
+                b[bIdx] = sum % 10;
+                carry = sum / 10;
+                aIdx--;
+                bIdx--;
+            }
+
+            if (carry >= 1) {
+                b = Arrays.copyOfRange(b, 0, b.length + 1);
+                for (int x = b.length - 1; x >= 1; x--) {
+                    b[x] = b[x - 1];
+                }
+                b[0] = carry;
+            }
+
+            return b;
+        }
+    }
+
+    public String getString(int[] num) {
+        if (num == null || num.length == 0) {
+            return null;
+        } else {
+            StringBuffer buf = new StringBuffer();
+            int sIdx = 0;
+
+            while (sIdx < num.length && num[sIdx] == 0) {
+                sIdx++;
+            }
+
+            while (sIdx < num.length) {
+                buf.append(num[sIdx]);
+                sIdx++;
+            }
+
+            if (buf.length() == 0) {
+                buf.append('0');
+            }
+
+            return buf.toString();
+
+        }
+    }
